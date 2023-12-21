832765a92cad1339a9cda3fcca9879a8813f1cc8
shivam-singh-negi <shivamsingh.comp1999@gmail.com>
2023-12-21 20:53:21 +0530

    Added Header and footer

---
+++ app/(root)/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from '@/components/ui/button'
+
+export default function Home() {
+  return (
+
+    <main className="text-red-800">Hi thers
+    
+    <Button>Hit me</Button>
+    </main>
+  
+  )
+}
